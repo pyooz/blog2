b2fb50124e0bd748c42e45f36fae19f134faad53
kirbyzzk <real_pyj@naver.com>
2021-04-25 18:16:02 +0900

    1st

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+$(function () {
+  // Slider
+  $('#coin-slider').coinslider({ width: 960, height: 360, opacity: 1 });
+});
+
+Cufon.replace('.menu_nav ul li a, span, h1, h2, h3, h4, h5, h6', { hover: true });
